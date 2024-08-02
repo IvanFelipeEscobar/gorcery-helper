76c52b4bfe7eba629cb260ac3ce7e34348800fe1
Ivan Escobar <ivnescbr1989@gmail.com>
2024-08-02 14:41:53 -0400

    api folder, lib folder

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <main className="">
-     add stuff here
+    <main className="flex flex-col justify-center items-center">
+      <div className="card">
+        <div className="card-title">Gorcery List</div>
+        <div className="card-body">
+          <ul>
+            <li>thing</li>
+            <li>stuff</li>
+            <li>boop</li>
+          </ul>
+        </div>
+      </div>
     </main>
   );
 }
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="font-extrabold text-2xl tracking-wide text-center py-2">
+    <header className="font-extrabold text-2xl tracking-wide text-center  my-3 border py-2">
       Grocery Helper
     </header>
   );
