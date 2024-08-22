6ebf4e4c34641c018ef0eb3d4274225994d3a5a7
Ivan Escobar <ivnescbr1989@gmail.com>
2024-08-21 23:22:51 -0400

    signin/register credentials -start

--- auth.ts
+++ auth.ts
@@ -1,5 +1,41 @@
-import NextAuth from "next-auth"
- 
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { z } from "zod";
+
+import { sql } from "@vercel/postgres";
+import { User } from "./lib/definitions";
+const getUser = async (email: string): Promise<User | undefined> => {
+  try {
+    const user = await sql<User>`SELECT * from USERS where email = ${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error("failed to fetch user: ", error);
+    throw new Error("failed to fetch user");
+  }
+};
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6),
+          })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const verifyPassword = await bcrypt.compare(password, user.password);
+          if (verifyPassword) return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+});
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { SignIn } from "../signun";
 
 const Header = () => {
   return (
     <header className="font-extrabold text-2xl tracking-wide text-center  my-3 border py-2">
       Grocery Helper
+      <div className="text-right"><SignIn/></div>
     </header>
   );
 };
